6651bb76b67bdfc53532e9a9de623b4c5eefc141
Inari <piotrek1723@gmail.com>
2023-07-17 22:39:18 +0200

    feat: create application runtime logic

--- application/public/index.php
+++ application/public/index.php
@@ -1,3 +1,14 @@
 <?php
 
-phpinfo();
+declare(strict_types=1);
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+use App\Kernel\Application;
+use App\Routes\Routes;
+
+$app = new Application();
+$routes = new Routes();
+$app->router = $routes->registerRoutes($app->router);
+
+$app->execute();
---
+++ application/src/Kernel/Application.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Kernel;
+
+use App\Kernel\Component\Response;
+use DI\Container;
+use App\Kernel\Router;
+use DI\ContainerBuilder;
+
+class Application
+{
+    public Container $container;
+    public Router $router;
+
+    public function __construct()
+    {
+        $this->router = new Router();
+        $this->setContainer();
+    }
+
+    private function setContainer(): void
+    {
+        $containerBuilder = new ContainerBuilder();
+        $containerBuilder->addDefinitions(dirname(__DIR__) . '/Container/config.php');
+        $containerBuilder->useAttributes(true);
+
+        $this->container = $containerBuilder->build();
+    }
+
+    public function execute(): void
+    {
+        $routeParams = $this->router->getRequestMatchingRouteParams();
+
+        if (empty($routeParams)) {
+            Response::notFound();
+        }
+    }
+}
