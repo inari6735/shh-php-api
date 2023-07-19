5570ca53fd003e9150b8125f2511c68a6ed514a1
Inari <piotrek1723@gmail.com>
2023-07-20 01:38:26 +0200

    feat: load .env file

--- application/public/index.php
+++ application/public/index.php
@@ -6,8 +6,12 @@ require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 use App\Config\Routes\Routes;
 use App\Kernel\Application;
+use Dotenv\Dotenv;
 
 $app = new Application();
 $app->router->registerRoutes();
+
+$dotEnv = Dotenv::createImmutable(dirname(__DIR__));
+$dotEnv->load();
 
 $app->execute();
