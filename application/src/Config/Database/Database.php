2abfaba7a13776f3c48b08a09a19eead194efb9a
Inari <piotrek1723@gmail.com>
2023-07-20 00:18:43 +0200

    chore: create Database config class

---
+++ application/src/Config/Database/Database.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Config\Database;
+
+use Doctrine\DBAL\DriverManager;
+use Doctrine\ORM\Configuration;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Mapping\Driver\AttributeDriver;
+use Symfony\Component\Cache\Adapter\ArrayAdapter;
+use Symfony\Component\Cache\Adapter\PhpFilesAdapter;
+
+class Database
+{
+    public function createEntityManager(): EntityManager
+    {
+        $entityFilesPaths = [__DIR__ . '/../../Entity'];
+        $applicationMode = 'development';
+
+        if ($applicationMode == 'development') {
+            $queryCache = new ArrayAdapter();
+            $metadataCache = new ArrayAdapter();
+        } else {
+            $queryCache = new PhpFilesAdapter('doctrine_queries');
+            $metadataCache = new PhpFilesAdapter('doctrine_metadata');
+        }
+
+        $config = new Configuration();
+        $config->setMetadataCache($metadataCache);
+        $driverImpl = new AttributeDriver($entityFilesPaths);
+        $config->setMetadataDriverImpl($driverImpl);
+        $config->setQueryCache($queryCache);
+        $config->setProxyDir(__DIR__ . '/Cache/Proxies');
+        $config->setProxyNamespace('App\Config\Database\Cache\Proxies');
+
+        if ($applicationMode == 'development') {
+            $config->setAutoGenerateProxyClasses(true);
+        } else {
+            $config->setAutoGenerateProxyClasses(false);
+        }
+
+        $connection = DriverManager::getConnection([
+            'url' => 'mysqli://root:root@mysql:3306/test'
+        ]);
+
+        return new EntityManager($connection, $config);
+    }
+}
