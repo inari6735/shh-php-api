a2b40aa668d208965058d4ed10073c601bd02585
inari <piotrek1723@gmail.com>
2023-07-31 00:56:38 +0200

    feat: add entity manager di

--- application/src/Config/Container/config.php
+++ application/src/Config/Container/config.php
@@ -2,5 +2,11 @@
 
 declare(strict_types=1);
 
+use App\Config\Database\Database;
+use Doctrine\ORM\EntityManagerInterface;
+
 return [
+    EntityManagerInterface::class => function (Database $database) {
+        return $database->createEntityManager();
+    }
 ];
