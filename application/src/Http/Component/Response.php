f8b1f9b5ed1d3d8b620b87664d7ec3565d3ec7e6
Piotr Nowak <piotr.nowak@creativestyle.pl>
2023-12-29 00:22:29 +0100

    feat: create new server config

--- application/src/Http/Component/Response.php
+++ application/src/Http/Component/Response.php
@@ -91,7 +91,7 @@ class Response
     }
 
     public static function setCorsPolicy(): void {
-        header("Access-Control-Allow-Origin: http://localhost:5173");
+        header("Access-Control-Allow-Origin: *");
         header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
         header("Access-Control-Allow-Headers: Content-Type, Authorization");
         header("Access-Control-Allow-Credentials: true");
