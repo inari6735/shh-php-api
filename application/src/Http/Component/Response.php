ae03ccb7a119c8a94778eeec3175d2949d5fdd5c
Piotr Nowak <piotr.nowak@creativestyle.pl>
2023-12-29 02:22:31 +0100

    fix: change cors

--- application/src/Http/Component/Response.php
+++ application/src/Http/Component/Response.php
@@ -91,7 +91,7 @@ class Response
     }
 
     public static function setCorsPolicy(): void {
-        header("Access-Control-Allow-Origin: http://ec2-52-59-161-128.eu-central-1.compute.amazonaws.com");
+        header("Access-Control-Allow-Origin: *");
         header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
         header("Access-Control-Allow-Headers: Content-Type, Authorization");
         header("Access-Control-Allow-Credentials: true");
